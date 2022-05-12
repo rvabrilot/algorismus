4d3756eb850ea30fe1529214f2919b3d007c1989
Rodrigo Vera Abrilot <rveraab.stefanini@bancochile.cl>
2022-05-12 11:24:40 -0400

    floyd-warshall

---
+++ dynamic/floyd-warshall.py
@@ -0,0 +1,56 @@
+# Floyd Warshall Algorithm in python
+#PSEUDO
+#n = no of vertices
+#A = matrix of dimension n*n
+#for k = 1 to n
+#    for i = 1 to n
+#        for j = 1 to n
+#            Ak[i, j] = min (Ak-1[i, j], Ak-1[i, k] + Ak-1[k, j])
+#return A
+
+# Time Complexity
+# There are three loops. Each loop has constant complexities. So, the time complexity of the Floyd-Warshall algorithm is O(n3).
+
+# Space Complexity
+# The space complexity of the Floyd-Warshall algorithm is O(n2).
+
+# Floyd Warshall Algorithm Applications
+# To find the shortest path is a directed graph
+# To find the transitive closure of directed graphs
+# To find the Inversion of real matrices
+# For testing whether an undirected graph is bipartite
+
+# The number of vertices
+nV = 4
+
+INF = 999
+
+
+# Algorithm implementation
+def floyd_warshall(G):
+    distance = list(map(lambda i: list(map(lambda j: j, i)), G))
+
+    # Adding vertices individually
+    for k in range(nV):
+        for i in range(nV):
+            for j in range(nV):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    print_solution(distance)
+
+
+# Printing the solution
+def print_solution(distance):
+    for i in range(nV):
+        for j in range(nV):
+            if(distance[i][j] == INF):
+                print("INF", end=" ")
+            else:
+                print(distance[i][j], end="  ")
+        print(" ")
+
+
+G = [[0, 3, INF, 5],
+         [2, 0, INF, 4],
+         [INF, 1, 0, INF],
+         [INF, INF, 2, 0]]
+floyd_warshall(G)
